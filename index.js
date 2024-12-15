1b5c31b91552baafa809a11557e5c507d94b160e
Victor Yuen <92613498+victoryuen@users.noreply.github.com>
2024-12-14 20:47:05 -0500

    First commit, intial server and package json

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+app = express();
+const hostname = "127.0.0.1";
+const port = 3000;
+
+app.get('/',(req,res) => {
+    res.send("Hello World");
+})
+
+app.listen(port,()=>{
+    console.log(`Express server listening on port ${port} `)
+})
+
+
